cc22b90616c9fc667c8a1c8bab125b537da6ee46
snailzyx <snailzyx@hotmail.com>
2016-04-10 01:06:59 +0800

    updateOrder2

---
+++ youqinghai/OrderList/OrderListModel.h
@@ -0,0 +1,13 @@
+//
+//  OrderListModel.h
+//  youqinghai
+//
+//  Created by snailz on 16/4/9.
+//  Copyright © 2016年 舒永超. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OrderListModel : NSObject
+
+@end
---
+++ youqinghai/OrderList/OrderListModel.m
@@ -0,0 +1,13 @@
+//
+//  OrderListModel.m
+//  youqinghai
+//
+//  Created by snailz on 16/4/9.
+//  Copyright © 2016年 舒永超. All rights reserved.
+//
+
+#import "OrderListModel.h"
+
+@implementation OrderListModel
+
+@end
