e284e407dc5d02a8a6ee23204d8adaa57809455f
hrx_hrx <276528553@qq.com>
2016-05-06 01:58:43 +0800

    修改orderNum订单号

--- youqinghai/OrderList/Controller/OrderDetailViewController.m
+++ youqinghai/OrderList/Controller/OrderDetailViewController.m
@@ -123,8 +123,8 @@
     NSString *mom = @"￥";
     
     
-    NSArray *values1 = @[_orderListMod.orderNum == nil ? @"" : _orderListMod.ordernum,_orderListMod.singletime,Statype];
-    NSArray *values2 = @[_orderListMod.tourName,_orderListMod.travelTime,[_orderListMod.travelnum stringByAppendingString:@"人"],[_orderListMod.dayNum stringByAppendingString:@"天"],_orderListMod.carTypeName,[mom stringByAppendingString:_orderListMod.orderMoney],[mom stringByAppendingString:_orderListMod.orderReserve]];
+    NSArray *values1 = @[_orderListMod.orderNum == nil ? @"" : _orderListMod.orderNum,_orderListMod.singletime,Statype];
+    NSArray *values2 = @[_orderListMod.tourName,_orderListMod.travelTime,[_orderListMod.orderNum stringByAppendingString:@"人"],[_orderListMod.dayNum stringByAppendingString:@"天"],_orderListMod.carTypeName,[mom stringByAppendingString:_orderListMod.orderMoney],[mom stringByAppendingString:_orderListMod.orderReserve]];
     NSArray *values3 = @[_orderListMod.nickname,_orderListMod.carname,_orderListMod.contacts,_orderListMod.contactsTel,_orderListMod.urgent,_orderListMod.urgentTel,_orderListMod.insuranceCost.integerValue?@"  是":@"  否"];
     [_datasourceOfValues addObject:values1];
     [_datasourceOfValues addObject:values2];
